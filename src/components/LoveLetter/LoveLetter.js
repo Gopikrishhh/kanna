1c1f8655b6853401af69dcbce61e8c88ed7e359d
Krishhh <gopik168@gmail.com>
2024-04-21 12:49:37 +0100

    first commit

---
+++ src/components/LoveLetter/LoveLetter.js
@@ -0,0 +1,50 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './Nee Valla O Pilla.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        Kannamma🖤,<br />
+        Nuvvante naku chaala istam entha ante cheppalenu okavela cheppina<br /> nenu explain chese way ki neeku ardam kaadhu<br />
+        In all the world there is no heart for me like yours In all the world there is no love for you like mine<br />
+		i loveeee youuuu  :)<br />
+        always yours,<br />
+        @Nee kannayya🖤
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
